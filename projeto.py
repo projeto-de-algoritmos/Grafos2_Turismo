6d439b1a79c5cbf77eebfaecd185d4df19f3045c
peHaick <pedrohaickzz@gmail.com>
2020-10-01 19:43:29 -0300

    Adicionando pontos turísticos às cidades

--- projeto.py
+++ projeto.py
@@ -4,15 +4,14 @@ cidades = {1: 'Ceilândia', 2: 'Samambaia', 3: 'Taguatinga', 4: 'Plano Piloto',
 }
 
 pontosTuristicos = {
-    13: 'Congresso Nacional', 14: 'Palácio do planalto', 15: 'Palácio Itamaraty',
+    13: 'Congresso Nacional', 14: 'Palácio do Planalto', 15: 'Palácio Itamaraty',
     16: 'Palácio da Alvorada',
     17: 'Biblioteca Nacional', 18: 'Ponte JK', 19: 'Torre de TV', 20: 'Pontão do Lago Sul',
     21: 'Jardim Botânico de Brasília',
     22: 'Parque Ecológico de Águas Claras', 23: 'Ermida Dom Bosco', 24: 'Memorial JK',
     25: 'Centro Cultural Banco do Brasil',
     26: 'Museu Nacional', 27: 'Museu do Catetinho',
-    28: 'Catedral Metropolitana de Brasília',
-    29: 'Igreja Dom Bosco'
+    28: 'Catedral Metropolitana de Brasília', 29: 'FGA'
 }
 
 grafo = {}
@@ -57,3 +56,22 @@ cidadeCidade(6, 9, 17)
 cidadeCidade(7, 2, 9)
 cidadeCidade(7, 8, 17)
 cidadeCidade(8, 11, 17)
+
+cidadePonto(4, 13, 4)
+cidadePonto(4, 14, 5)
+cidadePonto(4, 15, 4)
+cidadePonto(4, 16, 9)
+cidadePonto(4, 17, 4)
+cidadePonto(4, 18, 8)
+cidadePonto(4, 19, 4)
+cidadePonto(4, 20, 7)
+cidadePonto(4, 21, 16)
+
+cidadePonto(6, 22, 2)
+cidadePonto(4,23,17)
+cidadePonto(4,24,5)
+cidadePonto(4,25,9)
+cidadePonto(4,26,4)
+cidadePonto(8,27,18)
+cidadePonto(4,28,3)
+cidadePonto(8,29,5)
