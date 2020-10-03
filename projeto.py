9e35860325707ff3dea71e6210703de3f55d973c
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-10-02 22:29:49 -0300

    Adicionado lista de adjacencias

--- projeto.py
+++ projeto.py
@@ -1,8 +1,9 @@
 import heapq
 
-nodes = {1: 'Ceilândia', 2: 'Samambaia', 3: 'Taguatinga', 4: 'Plano Piloto',
-           5: 'Planaltina', 6: 'Águas Claras', 7: 'Recanto das Emas', 8: 'Gama', 
-           9: 'Guará', 10: 'Sobradinho', 11:'Park Way',
+nodes = {   
+    1: 'Ceilândia', 2: 'Samambaia', 3: 'Taguatinga', 4: 'Plano Piloto',
+    5: 'Planaltina', 6: 'Águas Claras', 7: 'Recanto das Emas', 8: 'Gama', 
+    9: 'Guará', 10: 'Sobradinho', 11:'Park Way',
     13: 'Congresso Nacional', 14: 'Palácio do Planalto', 15: 'Palácio Itamaraty',
     16: 'Palácio da Alvorada',
     17: 'Biblioteca Nacional', 18: 'Ponte JK', 19: 'Torre de TV', 20: 'Pontão do Lago Sul',
@@ -18,19 +19,19 @@ numVertices = 0
 
 
 def adcVertice(vertice):
-    global grafo
-    global numVertices
-    numVertices += 1
-    grafo[vertice] = []
+  global grafo
+  global numVertices
+  numVertices += 1
+  grafo[vertice] = []
 
 
 for cidade in nodes:
-    adcVertice(cidade)
+  adcVertice(cidade)
 
 def adcAresta(node1, node2, distancia):
-    global grafo
-    grafo[node1].append((node2, distancia))
-    grafo[node2].append((node1, distancia))
+  global grafo
+  grafo[node1].append((node2, distancia))
+  grafo[node2].append((node1, distancia))
 
 adcAresta(1, 2, 9)
 adcAresta(1, 3, 7)
@@ -42,7 +43,6 @@ adcAresta(4, 9, 15)
 adcAresta(4, 10, 23)
 adcAresta(5, 10, 22)
 adcAresta(6, 9, 17)
-adcAresta(7, 2, 9)
 adcAresta(7, 8, 17)
 adcAresta(8, 11, 17)
 adcAresta(11, 4, 15)
@@ -60,66 +60,65 @@ adcAresta(4,23,17)
 adcAresta(4,24,5)
 adcAresta(4,25,9)
 adcAresta(4,26,4)
-adcAresta(8,27,18)
+adcAresta(11,27,15)
 adcAresta(4,28,3)
 adcAresta(8,29,5)
 
-def menorCaminho(partida,destino,grafo = grafo):
-    h = []
-    ordem={}
-    heapq.heappush(h,(0,partida))
-    currvtx = partida
-    while len(h)!=0:
-        anterior = currvtx
-        currcost,currvtx = heapq.heappop(h)
-        ordem[anterior]=currvtx
-        if currvtx == destino:
-            print("Ponto de partida {} para o Destino {} tem a distancia de {} km".format(nodes[partida],nodes[destino],currcost))
-            break
-        for (neigh,neighcost) in grafo[currvtx]:
-            print(neigh, neighcost)
-            heapq.heappush(h,(currcost+neighcost,neigh))
+def printGrafo(grafo = grafo):
+  for node in range(1, 12):
+      print('[{}] {}:'.format(node, nodes[node]), end=' ')
+      for vizinho in grafo[node]:
+        if vizinho != grafo[node][-1]:
+            print('[{}] {}'.format(vizinho[0], nodes[vizinho[0]]), end=' - ')
+        else:
+            print('[{}] {}'.format(vizinho[0], nodes[vizinho[0]]))
+
+
+def dijkstra(partida,destino,grafo = grafo):
+  h = []
+  heapq.heappush(h,(0,partida))
+  while len(h)!=0:
+      currcost,currvtx = heapq.heappop(h)
+      if currvtx == destino:
+          print("Ponto de partida {} para o Destino {} tem a distancia de {} km".format(nodes[partida],nodes[destino],currcost))
+          break
+      for (neigh,neighcost) in grafo[currvtx]:
+          heapq.heappush(h,(currcost+neighcost,neigh))
+          print(neigh)
         
 def menu():
-    while True:
-        print('01. Listar Cidades')
-        print('02. Listar Pontos Turísticos')
-        print('03. Distancia entre cidade e ponto turístico')
-        print('04. Mostrar Caminho')
-        print('05. Mostrar Grafo')
-        print('06. Encerrar')
+  while True:
+      print('01. Listar Cidades')
+      print('02. Listar Pontos Turísticos')
+      print('03. Distancia entre cidade e ponto turístico')
+      print('04. Mostrar Lista de Adjacencias')
+      print('05. Encerrar')
 
-        func = int(input('Selecione uma função(1~6): '))
-        if func == 1:
-            print(' ')
-            for cidade in nodes:
-                print('[{}]: {}'.format(cidade, nodes[cidade]))
-                if cidade == 11:
-                    break
-            print(' ')
-        elif func == 2:
-            print(' ')
-            for x in range(13, 30):
-                print('[{}]: {}'.format(x, nodes[x]))
-            print(' ')
-        elif func == 3:
-            partida = int(input('Selecione a cidade de partida(1 - 11): '))
-            destino = int(input('Selecione o ponto turistico de destino(13 - 29): '))
-            print(' ')
-            menorCaminho(partida, destino)
-            # printCaminho(inicio, destino)
-            # print(' ')
-            # nx.draw_circular(G, node_color=color_map, with_labels=True)
-            # plt.show() # display
-        elif func == 4:
-            print(" ")
-            # printGrafo(grafo)
-            # print(" ")
-        elif func == 5:
-            print(" ")
-        elif func ==6:
-            return
-        else:
-            print('Função não definida!\n')
+      func = int(input('Selecione uma função(1~5): '))
+      if func == 1:
+          print(' ')
+          for cidade in nodes:
+              print('[{}]: {}'.format(cidade, nodes[cidade]))
+              if cidade == 11:
+                  break
+          print(' ')
+      elif func == 2:
+          print(' ')
+          for x in range(13, 30):
+              print('[{}]: {}'.format(x, nodes[x]))
+          print(' ')
+      elif func == 3:
+          partida = int(input('Selecione a cidade de partida(1 - 11): '))
+          destino = int(input('Selecione o ponto turistico de destino(13 - 29): '))
+          print(' ')
+          dijkstra(partida, destino)
+      elif func == 4:
+          print(" ")
+          printGrafo(grafo)
+          print(" ")
+      elif func ==5:
+          return
+      else:
+          print('Função não definida!\n')
 
 menu()
